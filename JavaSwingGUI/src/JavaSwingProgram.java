eabc736f7acdce020fc8ea5ad5b8d00408d3bf88
akihiro24 <111472710+akihiro24@users.noreply.github.com>
2023-03-14 10:46:24 +0800

    Delete JavaSwingGUI directory

--- JavaSwingGUI/src/JavaSwingProgram.java
+++
@@ -1,6 +0,0 @@
-public class JavaSwingProgram {
-    public static void main(String[] args) throws Exception {
-        JavaSwingGUI gui = new JavaSwingGUI();
-        gui.GUIProperties();
-    }
-}
